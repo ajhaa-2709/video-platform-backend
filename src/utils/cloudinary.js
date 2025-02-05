6e55033b9e7e64ea5c6f5530133a5f47ad8dcd18
ajhaa-2709 <139585348+ajhaa-2709@users.noreply.github.com>
2025-02-05 10:23:46 +0530

    Update cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,31 +1,43 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+import dotenv from "dotenv"
+import { console } from 'inspector';
+
+dotenv.config()
 
 
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
-        fs.unlinkSync(localFilePath)
-        return response;
+        if(!localFilePath) return null
 
+        const response = await cloudinary.uploader.upload(
+            localFilePath,{
+                resource_type:"auto"
+            }
+        )
+        console.log("File uploaded on cloudinary url "+ response.url)
+        fs.unlinkSync(localFilePath)
+        return response
     } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-        return null;
+       fs.unlinkSync(localFilePath)
+       return null 
+    }
+}
+
+const deleteFromCloudinary = async (publicId) => {
+    try {
+        const result= await cloudinary.uploader.destroy(publicId)
+        console.log("delting from cloudinary",publicId)
+    } catch (error) {
+        console.log("error deleting from cloudinary",error)
     }
 }
 
 
-
-export {uploadOnCloudinary}
+export {uploadOnCloudinary, deleteFromCloudinary}
